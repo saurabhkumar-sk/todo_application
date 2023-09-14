c1b7958226b39477be61d7713eebccaf4c4f98ab
saurabhkumar-sk <saurabhkumakairi501@gmail.com>
2023-09-14 17:13:16 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LandingScreen(title: 'Todo Application Home Page'),
+    );
+  }
+}
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({super.key, required this.title});
+  final String title;
+
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo'),
+      ),
+    );
+  }
+}
